2ac4f2c1892042a2191df32a991f2bf5108dc3e7
Alouiscious Media <info@reneesoul.com>
2020-01-20 05:04:49 -0800

    building seed and faker data

--- Gemfile
+++ Gemfile
@@ -20,9 +20,8 @@ gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 gem 'devise', '~> 4.7', '>= 4.7.1'
-
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
@@ -32,6 +31,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+28.times do
+  Musician.create(name: Faker::Name.unique.name, city: Faker::Address.city, email: Faker::Internet.email, phone: Faker::PhoneNumber.phone_number, instrument: Faker::Music.instrument)
+end
+
+10.times do
+  Rehearsal.create(location: 'Studio 9030', city: Faker::Address.city, purpose: "New Material", date: Faker::Date.between(from: 1.years.ago, to: Date.today), time: Faker::Time.forward(days: 180, period: :evening))
+  Rehearsal.create(location: 'BoomBah', city: Faker::Address.city, purpose: "New Musician", date: Faker::Date.forward(days: 180), time: Faker::Time.forward(days: 180, period: :evening))
+  Rehearsal.create(location: 'Toast', city: Faker::Address.city, purpose: "Special Event", date: Faker::Date.between(from: 1.years.ago, to: Date.today), time: Faker::Time.forward(days: 180, period: :evening))
+  Rehearsal.create(location: 'Northern Stars', city: Faker::Address.city, purpose: "Venue setup", date: Faker::Date.forward(days: 180), time: Faker::Time.forward(days: 180, period: :evening))
+
+end
+
+
+# Rehearsal.all.each_with_index do |rehearsal, i|
+#   rehearsal.students << [Student.limit(8).offset(i*2)]
+# end
