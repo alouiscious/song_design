95d2af7a5049f98a27b0d4dc0960f484b588e564
Alouiscious Media <info@reneesoul.com>
2020-01-28 20:37:57 -0800

    correct song and rehearsal index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  resources :user_rehearsals
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :rehearsals do
+    resources :songnotes, only: [:index, :show]
+
+  end
+
+  
   resources :rehearsals
   resources :users
   resources :songs
